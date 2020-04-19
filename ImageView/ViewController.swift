9d3cb6da64f60eed83b0147dc5f65211125432a8
Sohyun <carriesohyun@gmail.com>
2020-04-19 22:45:50 +0900

    Light on/off and magnify/minimize light bulb

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
 
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+
     }
 
 
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat=2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if(isZoom)
+        {
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("Magnify",for: .normal)
+        }
+        else
+        {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("Minimize",for: .normal)
+        }
+        
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn
+        {
+            imgView.image = imgOn
+        }
+        else
+        {
+            imgView.image = imgOff
+        }
+    }
 }
 
